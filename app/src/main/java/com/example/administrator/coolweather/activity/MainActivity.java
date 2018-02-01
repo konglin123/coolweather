b293a1cd732788b08fee562ada14ce640fb13a60
konglin123 <1290242450@qq.com>
2018-02-01 17:07:04 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/administrator/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.administrator.coolweather;
+package com.example.administrator.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.administrator.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
